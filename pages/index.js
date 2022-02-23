6f03b71340afb7a62fe50b2a14e8b2d6dcbe7aeb
kwonsean <gownn12@naver.com>
2022-02-23 22:55:00 +0900

    feat: Update file to server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch("/api/movies")).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
+
   return (
     <div className="container">
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -41,4 +42,15 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// 함수명 무조건 이렇게 작성 필수!
+// 이곳에 작성된 코드는 서버쪽에서만 작동됨
+export async function getServerSideProps() {
+  const { results } = await (await fetch("http://localhost:3000/api/movies")).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
