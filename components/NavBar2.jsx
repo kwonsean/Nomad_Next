9f5c1d094d6e1cdaa558d9859a743aa2fc094be1
kwonsean <gownn12@naver.com>
2022-02-23 22:22:15 +0900

    feat: Update navBar2 component

--- components/NavBar2.jsx
+++ components/NavBar2.jsx
@@ -4,28 +4,44 @@ import { useRouter } from "next/router";
 export default function NavBar2() {
   const router = useRouter();
   return (
-    <>
-      <nav>
+    <nav>
+      <img src="/vercel.svg" />
+      <div>
         <Link href="/">
-          <a className={router.pathname === "/" ? "active" : ""}>HOME</a>
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
         </Link>
         <Link href="/about">
-          <a className={router.pathname === "/about" ? "active" : ""}>ABOUT</a>
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
         </Link>
-      </nav>
+      </div>
       <style jsx>
         {`
           nav {
-            background-color: purple;
+            display: flex;
+            gap: 10px;
+            flex-direction: column;
+            align-items: center;
+            padding-top: 20px;
+            padding-bottom: 10px;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
           }
-          a {
-            color: white;
+          img {
+            max-width: 100px;
+            margin-bottom: 5px;
+          }
+          nav a {
+            font-weight: 600;
+            font-size: 18px;
           }
           .active {
-            color: yellow;
+            color: tomato;
+          }
+          nav div {
+            display: flex;
+            gap: 10px;
           }
         `}
       </style>
-    </>
+    </nav>
   );
 }
