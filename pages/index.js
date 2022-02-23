6c80774e184d6630a7328516afa23cd4df53d5f0
kwonsean <gownn12@naver.com>
2022-02-23 22:27:12 +0900

    feat: Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch("/api/movies")).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
-    <>
-      <h1>HELLO!! This is home page</h1>
-      <h3>{counter}</h3>
-      <button onClick={() => setCounter((cur) => cur + 1)}>+</button>
-    </>
+    <div className="container">
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          cursor: pointer;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 }
