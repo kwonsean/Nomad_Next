d765a7abd404c51067ed284d6b5a31c9c40b14cb
kwonsean <gownn12@naver.com>
2022-02-24 16:52:38 +0900

    feat: Delete Seo component

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import { useRouter } from "next/router";
 import Layout from "../components/Layout";
-import Seo from "../components/Seo";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  let title = router.pathname.slice(1).toUpperCase();
-  if (!title) title = "HOME";
   return (
     <>
-      <Seo title={title} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
