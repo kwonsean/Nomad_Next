7e6dd8638b31af8c732fbabad9d4c320b8da8ad1
kwonsean <gownn12@naver.com>
2022-02-24 18:49:38 +0900

    feat: Add click event

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
 export default function Home({ results }) {
-  const [movies, setMovies] = useState();
+  const router = useRouter();
+
+  const handleClickPoster = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${title}/${id}`,
+    );
+  };
+  // const [movies, setMovies] = useState();
   // useEffect(() => {
   //   (async () => {
   //     const { results } = await (await fetch("/api/movies")).json();
@@ -11,11 +27,16 @@ export default function Home({ results }) {
 
   return (
     <div className="container">
+      <Seo title="HOME" />
       {/* {!movies && <h4>Loading...</h4>} */}
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div className="movie" key={movie.id} onClick={() => handleClickPoster(movie.id, movie.title)}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link href={`/movies/${movie.title}/${movie.id}`}>
+              <a>{movie.title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
